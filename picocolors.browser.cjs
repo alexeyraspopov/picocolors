fd3461b580946d852d6aee570f89c7be3c5c596d
Alexey Raspopov <alexey.raspopov@datarobot.com>
2021-09-27 12:53:39 -0400

    add minimum browser support

---
+++ picocolors.browser.cjs
@@ -0,0 +1,2 @@
+function x(v) { return v; }
+module.exports = { isColorSupported: false, reset: x, bold: x, dim: x, italic: x, underline: x, inverse: x, hidden: x, strikethrough: x, black: x, red: x, green: x, yellow: x, blue: x, magenta: x, cyan: x, white: x, gray: x, bgBlack: x, bgRed: x, bgGreen: x, bgYellow: x, bgBlue: x, bgMagenta: x, bgCyan: x, bgWhite: x };
---
+++ picocolors.browser.js
@@ -0,0 +1,3 @@
+function x(v) { return v; }
+let isColorSupported = false;
+export { isColorSupported, x as reset, x as bold, x as dim, x as italic, x as underline, x as inverse, x as hidden, x as strikethrough, x as black, x as red, x as green, x as yellow, x as blue, x as magenta, x as cyan, x as white, x as gray, x as bgBlack, x as bgRed, x as bgGreen, x as bgYellow, x as bgBlue, x as bgMagenta, x as bgCyan, x as bgWhite };
