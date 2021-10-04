cd81dd059842ea07be4476456d3c6048b407a48c
Andrey Sitnik <andrey@sitnik.ru>
2021-10-04 10:53:14 +0200

    Allow to import Colors and Formatter

--- picocolors.d.ts
+++ picocolors.d.ts
@@ -1,33 +1,4 @@
-type Formatter = (input: string | number | null | undefined) => string
-
-interface Colors {
-	isColorSupported: boolean
-	reset: Formatter
-	bold: Formatter
-	dim: Formatter
-	italic: Formatter
-	underline: Formatter
-	inverse: Formatter
-	hidden: Formatter
-	strikethrough: Formatter
-	black: Formatter
-	red: Formatter
-	green: Formatter
-	yellow: Formatter
-	blue: Formatter
-	magenta: Formatter
-	cyan: Formatter
-	white: Formatter
-	gray: Formatter
-	bgBlack: Formatter
-	bgRed: Formatter
-	bgGreen: Formatter
-	bgYellow: Formatter
-	bgBlue: Formatter
-	bgMagenta: Formatter
-	bgCyan: Formatter
-	bgWhite: Formatter
-}
+import { Colors } from "./types"
 
 declare const picocolors: Colors & { createColors: (enabled: boolean) => Colors }
 
