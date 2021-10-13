cd0b670a129331d9faf2717bb76e608e569e0b3e
Alexey Raspopov <alexey.raspopov@datarobot.com>
2021-10-13 10:11:18 -0400

    rework loading benchmark to avoid v8 caching

--- benchmarks/loading-runner.js
+++ benchmarks/loading-runner.js
@@ -1,39 +1,58 @@
-const { performance } = require("perf_hooks")
+let { performance } = require("perf_hooks")
+let { execSync } = require("child_process")
 
+let path
 let before
+let baseline
 function showTime(name) {
 	let after = performance.now()
-	process.stdout.write(name + " " + (after - before) + "\n")
+	process.stdout.write(name + " " + (after - before - baseline) + "\n")
 }
 
+let startBaseline = performance.now()
+execSync('node --eval ""')
+baseline = performance.now() - startBaseline
+
+path = require.resolve("chalk")
 before = performance.now()
-let chalk = require("chalk")
+load(path)
 showTime("chalk")
 
+path = require.resolve("cli-color")
 before = performance.now()
-let cliColor = require("cli-color")
+load(path)
 showTime("cli-color")
 
+path = require.resolve("ansi-colors")
 before = performance.now()
-let ansi = require("ansi-colors")
+load(path)
 showTime("ansi-colors")
 
+path = require.resolve("kleur")
 before = performance.now()
-let kleur = require("kleur")
+load(path)
 showTime("kleur")
 
+path = require.resolve("kleur/colors")
 before = performance.now()
-let kleurColors = require("kleur/colors")
+load(path)
 showTime("kleur/colors")
 
+path = require.resolve("colorette")
 before = performance.now()
-let colorette = require("colorette")
+load(path)
 showTime("colorette")
 
+path = require.resolve("nanocolors")
 before = performance.now()
-let nanocolors = require("nanocolors")
+load(path)
 showTime("nanocolors")
 
+path = require.resolve("../picocolors.js")
 before = performance.now()
-let picocolors = require("../picocolors.js")
+load(path)
 showTime("picocolors")
+
+function load(path) {
+	execSync(`node --eval 'require("${path}")'`)
+}
