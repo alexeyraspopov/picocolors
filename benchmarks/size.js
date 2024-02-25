e50360d54afca3393d1d736e219e8b4c3f4ced67
Ferdinand Prantl <prantlf@gmail.com>
2024-02-25 13:48:02 +0100

    Print package sizes for both chalk 4 and 5

--- benchmarks/size.js
+++ benchmarks/size.js
@@ -34,7 +34,8 @@ async function benchmark(lib) {
 
 async function start() {
 	process.stdout.write(gray("Data from packagephobia.com\n"))
-	await benchmark("  chalk")
+	await benchmark("  chalk@5.3.0")
+	await benchmark("  chalk@4.1.2")
 	await benchmark("  cli-color")
 	await benchmark("  ansi-colors")
 	await benchmark("  kleur")
